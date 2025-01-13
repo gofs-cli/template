e8b05b125fa5ce0c48939b8af17e91f2e5a9930f
Steven Lee <steven.lee@kynrai.com>
2025-01-13 16:30:19 +0000

    feat: add modern normalize

--- internal/ui/index_templ.go
+++ internal/ui/index_templ.go
@@ -29,7 +29,7 @@ func IndexPage(page templ.Component) templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<!doctype html><html lang=\"en\"><meta charset=\"UTF-8\"><title>gofs</title><meta name=\"viewport\" content=\"width=device-width,initial-scale=1\"><link rel=\"stylesheet\" href=\"/assets/css/styles.css\"><!-- HTMX Core --><script defer src=\"/assets/js/htmx.min.js\"></script><!-- HTMX Plugins --><script defer src=\"/assets/js/response-targets.js\"></script><!-- Alpine Plugins --><script defer src=\"/assets/js/persist.js\"></script><script defer src=\"/assets/js/collapse.js\"></script><!-- Alpine Core --><script defer src=\"/assets/js/alpine.js\"></script><body hx-ext=\"response-targets\" hx-target-error=\"this\">")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<!doctype html><html lang=\"en\"><meta charset=\"UTF-8\"><title>gofs</title><meta name=\"viewport\" content=\"width=device-width,initial-scale=1\"><link rel=\"stylesheet\" href=\"/assets/css/modern-normalize.css\"><link rel=\"stylesheet\" href=\"/assets/css/styles.css\"><!-- HTMX Core --><script defer src=\"/assets/js/htmx.min.js\"></script><!-- HTMX Plugins --><script defer src=\"/assets/js/response-targets.js\"></script><!-- Alpine Plugins --><script defer src=\"/assets/js/persist.js\"></script><script defer src=\"/assets/js/collapse.js\"></script><!-- Alpine Core --><script defer src=\"/assets/js/alpine.js\"></script><body hx-ext=\"response-targets\" hx-target-error=\"this\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
